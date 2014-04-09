4127ea9aa0990dd47e67884275ef5481bd3f1ed3
funkayMan <tyler.j.arsenault@gmail.com>
2014-04-09 14:40:19 -0400

    The file called SuperBruteForceIt.m works, but it does have some B.c. Issues me thinks.

--- ValidationScripts/TestMethods/SuperBruteForceIt.m
+++ ValidationScripts/TestMethods/SuperBruteForceIt.m
@@ -1,6 +1,8 @@
-ccc
+clf
+clear
+clc
 
-N=3;
+N=10;
 N1=N+1;
 [phi, D, x, w,gamma]=GLL_Basis(N);
 
@@ -11,58 +13,40 @@ for m = 1:N1
         M((n-1)*N1+m,(n-1)*N1+m)=w(m)*w(n);
     end
 end
-
+P=zeros(N1*N1);
+K=P;
 for m = 1:N1
     for n = 1:N1
         i1=(m-1)*N1+n;
-        for i = 1:N1
-            for j = 1:N1
-                i2=(i-1)*N1+j;
+        for j = 1:N1
+            for k = 1:N1
+                i2=(j-1)*N1+k;
                 for xi = 1:N1
                     for eta = 1:N1
-                        tmp=(phi(j,eta)*D(i,xi)+phi(i,xi)*D(j,eta))*(phi(m,xi)*D(n,eta)+phi(n,eta)*D(m,xi))+...
-                            (phi(i,xi)*D(j,eta)+phi(j,eta)*D(i,xi))*(phi(m,xi)*D(n,eta)+phi(n,eta)*D(m,xi));
-                        K(i1,i2)=-tmp*w(xi)*w(eta);
+                        P(i1,i2)=P(i1,i2)+1;
+% %                         tmp=(phi(j,eta)*D(i,xi)+phi(i,xi)*D(j,eta))*(phi(m,xi)*D(n,eta)+phi(n,eta)*D(m,xi))+...
+% %                             (phi(i,xi)*D(j,eta)+phi(j,eta)*D(i,xi))*(phi(m,xi)*D(n,eta)+phi(n,eta)*D(m,xi));
+
+                        tmp =phi(m,eta)*D(n,xi)*phi(k,eta)*D(j,xi)+phi(n,xi)*D(m,eta)*phi(j,xi)*D(k,eta);
+                        K(i1,i2)=-tmp*w(xi)*w(eta)+K(i1,i2);
                     end
                 end
             end
         end
     end
 end
-K
+% K
 
 %% Boundary Conditions
-% K(1:N1,:)=0;
-% K(N1*(N1-1)+1:N1*N1,:)=0;
-% K(1:N1,1:N1)=eye(N1);
-% K(N1*(N1-1)+1:N1*N1,N1*(N1-1)+1:N1*N1)=eye(N1);
-% 
-% for row=2:N
-%     for col=1:N1
-%         if row == col
-%             K((row-1)*N1+1,((col-1)*N1+1):((col-1)*N1+N1))=0;
-%             K((row-1)*N1+1,(col-1)*N1+1)=1;
-%             K((row-1)*N1+N1,((col-1)*N1+1):((col-1)*N1+N1))=0;
-%             K((row-1)*N1+N1,(col-1)*N1+N1)=1;
-%         else
-%             K((row-1)*N1+1,(col-1)*N1+1)=0;
-%             K((row-1)*N1+1,(col-1)*N1+N1)=0;
-%             K((row-1)*N1+N1,(col-1)*N1+1)=0;
-%             K((row-1)*N1+N1,(col-1)*N1+N1)=0;
-%         end
-%     end
-% end
+K(1:N1,:)=0;
+K(N1*(N1-1)+1:N1*N1,:)=0;
+K(1:N1,1:N1)=eye(N1);
+K(N1*(N1-1)+1:N1*N1,N1*(N1-1)+1:N1*N1)=eye(N1);
 
-
-
-
-
-
-
-
-
-
-
+for row=2:N
+    K((row-1)*N1+1,:)=0;
+    K((row-1)*N1+N1,:)=0;
+end
 
 
 f=sin(pi/2.*xx-pi/2).*sin(pi/2.*yy-pi/2);
@@ -74,8 +58,8 @@ end
 % surf
 
 %% Time integration
-T=5;
-dt=0.01;
+T=0.05;
+dt=0.0001;
 t=0:dt:T;
 n=length(t);
 u_iterat=zeros(N1*N1,n);
@@ -91,12 +75,12 @@ fprintf('Time elapsed for time integration : %6.5f',toc);
 % Build u back into f for each timestep
 % plot them
 %% Plot it.
-for i = 1:floor(n/5):n
+for i = 1:n
     surfc(xx,yy,reshape(u_iterat(:,i),[N1 N1]));
-%     zlim([0 1])
+    zlim([0 1])
     xlabel('x');
     ylabel('y');
     zlabel('z');
     title(['t = ' num2str(dt*i)]);
-    pause(0.1)
+    pause(0.001)
 end
